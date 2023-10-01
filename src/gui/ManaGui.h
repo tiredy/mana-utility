28ebc739b67eba3eee33fc172b9cab0619d79373
tiredy <@>
2023-10-01 10:53:15 +0200

    Add copyright header(s)

--- src/gui/ManaGui.h
+++ src/gui/ManaGui.h
@@ -1,3 +1,19 @@
+
+// Copyright (c) 2023. tiredy
+//
+// this program is free software: you can redistribute it and/or modify
+// it under the terms of the gnu affero general public license as
+// published by the free software foundation, either version 3 of the
+// license, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but without any warranty; without even the implied warranty of
+// merchantability or fitness for a particular purpose. See the
+// gnu affero general public license for more details.
+//
+// You should have received a copy of the gnu affero general public license
+// along with this program. If not, see <https: //www. Gnu. Org/licenses/>
+
 #pragma once
 
 #include <cmath>
